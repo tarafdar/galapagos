d98ea1a27baa7bd709d5099b13707fa09881c750
Smith  Quinn Werker <smithqu1@ug239.eecg.toronto.edu>
2018-05-03 13:37:54 -0400

    removed extra files created from synthesis

--- hlsIP_adm-8k5/axiStreamGate/vivado_hls.app
+++
@@ -1,9 +0,0 @@
-<AutoPilot:project xmlns:AutoPilot="com.autoesl.autopilot.project" projectType="C/C++" name="axiStreamGate" top="axiStreamGate">
-    <files>
-        <file name="../hlsSources/srcs/axiStreamGate.cpp" sc="0" tb="false" cflags=""/>
-    </files>
-    <solutions>
-        <solution name="solution1" status=""/>
-    </solutions>
-</AutoPilot:project>
-
--- hlsIP_adm-8k5/reverseEndian64/solution1/.autopilot/db/pragma.status.tcl
+++
@@ -1,3 +0,0 @@
-lappend pragmaStatus0 {pragma "INTERFACE" source "/nfs/ug/thesis/thesis0/pc/Quinn/galapagos/hlsIP_adm-8k5/../hlsSources/srcs/reverseEndian64.cpp" location "reverseEndian64" found 1 funcFound 1 applied 1 ambiguous 0 reason "null"}
-lappend pragmaStatus1 {pragma "INTERFACE" source "/nfs/ug/thesis/thesis0/pc/Quinn/galapagos/hlsIP_adm-8k5/../hlsSources/srcs/reverseEndian64.cpp" location "reverseEndian64" found 1 funcFound 1 applied 1 ambiguous 0 reason "null"}
-lappend pragmaStatus2 {pragma "INTERFACE" source "/nfs/ug/thesis/thesis0/pc/Quinn/galapagos/hlsIP_adm-8k5/../hlsSources/srcs/reverseEndian64.cpp" location "reverseEndian64" found 1 funcFound 1 applied 1 ambiguous 0 reason "null"}
