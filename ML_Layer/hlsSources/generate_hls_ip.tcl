aab13c5da48b5bd91cf971a5612e0efdd42cb71c
tarafdar <naif.tarafdar@mail.utoronto.ca>
2018-05-29 21:18:52 -0400

    added ML_Layer kernels

---
+++ ML_Layer/hlsSources/generate_hls_ip.tcl
@@ -0,0 +1,18 @@
+#Author: Naif Tarafdar
+
+cd userIP
+
+set MPI_HEADERS_DIR "../HMPI/HLS_lib/"
+
+open_project dariusWrapperHLS  
+set_top dariusWrapper
+add_files ../ML_Layer/hlsSources/dariusWrapper.cpp -cflags "-I ${MPI_HEADERS_DIR} "
+open_solution "solution1"
+set_part {xcku115-flva1517-2-e}
+create_clock -period 5 -name default
+config_interface -m_axi_addr64 -m_axi_offset off -register_io off -expose_global=true
+csynth_design
+export_design -rtl verilog -format ip_catalog
+close_project
+
+
---
+++ ML_Layer/ipPackage/package_top.tcl
@@ -0,0 +1,16 @@
+create_project darius_wrapper_ip ML_Layer/projects -part xcku115-flva1517-2-e
+
+set_property  ip_repo_paths  {./PYNQ-DL/darius/ip ./userIP} [current_project]
+update_ip_catalog
+
+create_bd_design darius_wrapper_bd
+open_bd_design ./ML_layer/projects/darius_wrapper_ip/darius_wrapper_ip.srcs/sources_1/bd/darius_wrapper_bd/darius_wrapper_bd.bd
+source ./ML_Layer/ipPackage/recreate_bd.tcl
+
+ipx::package_project -root_dir ./userIP/dariusWrapper -vendor user.org -library user -taxonomy /UserIP -module darius_wrapper_bd -import_files
+set_property core_revision 2 [ipx::find_open_core user.org:user:darius_wrapper_bd:1.0]
+ipx::create_xgui_files [ipx::find_open_core user.org:user:darius_wrapper_bd:1.0]
+ipx::update_checksums [ipx::find_open_core user.org:user:darius_wrapper_bd:1.0]
+ipx::save_core [ipx::find_open_core user.org:user:darius_wrapper_bd:1.0]
+close_project
+file delete -force ./ML_Layer/projects
