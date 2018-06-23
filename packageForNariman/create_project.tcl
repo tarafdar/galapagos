d314858fbd2ad45225dbf0b854cd8560295d1a53
tarafdar <naif.tarafdar@mail.utoronto.ca>
2018-06-22 20:56:53 -0400

    more cleaning

--- packageForNariman/create_project.tcl
+++
@@ -1,6 +0,0 @@
-create_project sample packageForNariman/ -part xcku115-flva1517-2-e
-        
-set_property  ip_repo_paths  {./hlsIP_adm-8k5} [current_project]
-update_ip_catalog
-
-create_bd_design sample_bd 
