5aeb9a4f215edd72bf4235715693e2b151497a19
Nariman Eskandari <eskandarinariman@gmail.com>
2018-07-04 20:14:02 +0000

    fixed makefile

--- tclScripts/generate_hls_ip_middleware.tcl
+++ tclScripts/generate_hls_ip_middleware.tcl
@@ -4,21 +4,21 @@ set partName [lindex $argv 1]
 cd hlsIP_${boardName}
 
 
-##creates special switch for ip packets
-#open_project ethernet_bridge 
-#set_top ethernet_bridge
-#add_files ../hlsSources/srcs/ethernet_bridge.cpp
-##add_files -tb ../hlsSources/testBench/ethernet_bridge_tb.cpp
-#open_solution "solution1"
-##set_part {xcku115-flva1517-2-e}
-#set_part ${partName}
-#create_clock -period 250MHz -name default
-#config_interface -expose_global
-##csim_design -compiler gcc
-#csynth_design
-##cosim_design
-#export_design -format ip_catalog
-#close_project
+#creates special switch for ip packets
+open_project ethernet_bridge 
+set_top ethernet_bridge
+add_files ../hlsSources/srcs/ethernet_bridge.cpp
+#add_files -tb ../hlsSources/testBench/ethernet_bridge_tb.cpp
+open_solution "solution1"
+#set_part {xcku115-flva1517-2-e}
+set_part ${partName}
+create_clock -period 250MHz -name default
+config_interface -expose_global
+#csim_design -compiler gcc
+csynth_design
+#cosim_design
+export_design -format ip_catalog
+close_project
 
 #creates special switch for ip packets
 open_project TCP_output_bridge 
