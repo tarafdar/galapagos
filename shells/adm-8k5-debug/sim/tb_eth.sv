ebba02680132ae91da36e2894dfdc516f5f21d4f
Quinn Smith <quinn.smith98@gmail.com>
2018-07-19 15:35:35 +0000

    integrated our code with naif's code
    next step, actually simulate the thing

--- shells/adm-8k5-debug/sim/tb_eth.sv
+++ shells/adm-8k5-debug/sim/tb_eth.sv
@@ -16,26 +16,6 @@ typedef struct packed {
 
 import "DPI-C" function void parseJSON(input string jsonFilePath, inout packet extPacketList[], inout int ver, inout int logToFile);
 
-
-
-initial begin
-    input string jsonFilePath = "../jsonTests/jsonPacketTest_extensive.json";
-    inout packet [63:0] extPacketList;
-    inout int ver = 1'd0;
-    inout int logToFile = 1'd0;
-    parseJSON(jsonFilePath, extPacketList, ver, logToFile);
-end
-
-// Generate transaction for each flit
-initial begin
-    #50
-    for(int i = 0; i < $size(extPacketList); ++i) {
-        for(int j = 0; j < $size(extPacketList[i]); ++j) {
-            gen_transaction(i, extPacketList[i][j].data, extPacketList[i][j].keep, extPacketList[i][j].last);
-        }
-    }
-end
-
 module eth_stimulate
                     (
                      output reg [63:0] stream_out_DATA,
@@ -54,14 +34,23 @@ module eth_stimulate
     parameter [47:0] MAC_ADDR_FPGA = 48'hfa163e55ca02; 
     parameter [47:0] MAC_ADDR_STIM = 48'h0cc47a88c047; 
     
-
     initial begin
+        input string jsonFilePath = "../jsonTests/jsonPacketTest_extensive.json";
+        inout packet [63:0] extPacketList;
+        inout int ver = 1'd0;
+        inout int logToFile = 1'd0;
+        parseJSON(jsonFilePath, extPacketList, ver, logToFile);
+    
         stream_in_READY=1;           
         #50
         ethernet_header(.mac_addr_dst(MAC_ADDR_FPGA),
                                   .mac_addr_src(MAC_ADDR_STIM), 
                                   .dst(8'h00)
                                   );
+        
+        
+        
+        /*
         gen_transaction(.data(64'h0100000100030000), 
                         .keep(8'hff), 
                         .last(1'b0)
@@ -70,6 +59,16 @@ module eth_stimulate
                         .keep(8'h0f), 
                         .last(1'b1)
                     );
+        */
+
+        //generate a transaction for each flit
+        for(int i = 0; i < $size(extPacketList); ++i) {
+            for(int j = 0; j < $size(extPacketList[i]); ++j) {
+                gen_transaction(i, extPacketList[i][j].data, extPacketList[i][j].keep, extPacketList[i][j].last);
+            }
+        }
+
+
          
     end
     
